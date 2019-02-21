b02e3e6e964a406ccd7f6f712edbb9c585e8f7d2
Spencer Kelley <kelle881@umn.edu>
2019-02-21 15:08:56 -0500

    Setup appropriate parameters for xCell

--- xCell/runXCell.R
+++ xCell/runXCell.R
@@ -1,3 +1,3 @@
 library(xCell)
 exprMatrix = read.table('/data/RNAMat.txt',header=TRUE,row.names=1, as.is=TRUE)
-xCellAnalysis(exprMatrix)
+xCellAnalysis(exprMatrix, file.name = xCellOut.txt, parallel.sz = 16, parallel.type = FORK, rnaseq = TRUE)
