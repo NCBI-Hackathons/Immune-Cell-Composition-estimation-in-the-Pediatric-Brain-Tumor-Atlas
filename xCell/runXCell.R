ac46e01d19231c4d485a96c2e623c014d2b1c845
Spencer Kelley <kelle881@umn.edu>
2019-02-21 15:22:29 -0500

    Set appropriate hard-coded output file

--- xCell/runXCell.R
+++ xCell/runXCell.R
@@ -1,3 +1,3 @@
 library(xCell)
 exprMatrix = read.table('/data/RNAMat.txt',header=TRUE,row.names=1, as.is=TRUE)
-xCellAnalysis(exprMatrix, file.name = xCellOut.txt, parallel.sz = 16, parallel.type = FORK, rnaseq = TRUE)
+xCellAnalysis(exprMatrix, file.name = '/data/xCellOut.txt', parallel.sz = 16, parallel.type = FORK, rnaseq = TRUE)
