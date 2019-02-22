f4589e1f2e4e6ea61d4ded273ba475f32db7a40f
ramanp <ramanp@email.chop.edu>
2019-02-22 09:21:32 -0500

    updated Comparison code and format Code

---
+++ code/formatxCellOut.R
@@ -0,0 +1,23 @@
+############################################################
+#Purpose: Code to format xCell Output Columns
+#Author: Pichai Raman
+#Date: 2/21/2019
+############################################################
+
+args <- commandArgs(trailingOnly = TRUE)
+
+
+#################################################
+#Load Packages and Scripts
+#################################################
+
+#Libraries
+library("tidyverse");
+
+#Read in data
+tmpData <- read.delim(args[1]);
+colnames(tmpData) <- gsub("X", "", colnames(tmpData));
+colnames(tmpData) <- gsub("\\.", "-", colnames(tmpData));
+rownames(tmpData) <- tmpData[,1];
+tmpData <- tmpData[-1];
+write.table(tmpData, paste(gsub("\\.txt", "", args[1]), "formatted.txt", sep=""), col.names=T, row.names=T, sep="\t");
