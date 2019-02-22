ac2c1caa5407a0283a54fc3e2ddc01e8484f48c7
Spencer Kelley <kelle881@umn.edu>
2019-02-22 09:17:43 -0500

    Introduce basic imsig script

---
+++ code/imsig/runImsig.R
@@ -0,0 +1,23 @@
+library("optparse")
+library("imsig")
+
+option_list = list(
+  make_option(c("-i", "--input"), type="character", default=NULL,
+              help="dataset file name", metavar="in.txt"),
+  make_option(c("-o", "--out"), type="character", default=NULL, 
+              help="output file name", metavar="out.txt")
+);
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+if (is.null(opt$input) || is.null(opt$out)){
+  print_help(opt_parser)
+  stop("Input and output must be supplied", call.=FALSE)
+}
+
+exp = read.table(opt$input, header = T, row.names = 1, sep = '\t')
+imsig(exp = exp, r = 0.7)
+plot_abundance(exp = exp, r = 0.7)
+output <- imsig(exp = exp, r = 0.7)
+write.table(output, opt$out, sep="\t", row.names=T)
